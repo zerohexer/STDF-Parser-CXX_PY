09064a0a68dd5703d5373c08de3582eb16c80fc2
AdminPEN.AAn <zerohexer@gmail.com>
2025-08-19 16:33:08 +0800

    Clean up

--- run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Cross-platform STDF parser test launcher
-
-# Set library path for Linux/WSL
-export LD_LIBRARY_PATH="$PWD/cpp/third_party/lib:$LD_LIBRARY_PATH"
-
-# Run the test
-python3 test_parser.py "$@"
